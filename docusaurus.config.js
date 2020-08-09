17ac6c3e5f4d8b858bec99d316b80795382042e0
anshul <anshulgoel151999@gmail.com>
2020-08-09 16:44:58 +0530

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,87 @@
+module.exports = {
+  title: "Manan",
+  tagline: "A Techno Surge",
+  url: "https://manantechnosurge.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  favicon: "img/logo.png",
+  organizationName: "manan", // Usually your GitHub org/user name.
+  projectName: "club-website", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "Manan",
+      logo: {
+        alt: "Manan",
+        src: "img/logo.png",
+      },
+      items: [
+        { to: "event", label: "Events", position: "left" },
+        {
+          href: "https://github.com/Manan-YMCA",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Secy",
+          items: [
+            {
+              label: "Anshul",
+              href: "https://www.anshulgoyal.dev",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Linkedin",
+              href: "https://www.linkedin.com/company/manan-ymca/",
+            },
+            {
+              label: "Reddit",
+              href: "https://www.reddit.com/r/manantechnosurge/",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/manantechsurge",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Events",
+              to: "events",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/Manan-YMCA",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Manan, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        blog: {
+          showReadingTime: false,
+          path: "./event",
+          routeBasePath: "/event",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
